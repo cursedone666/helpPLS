6c97ece047c95982151af13d09ec7adb3f8d031a
Ilya <webfox02@gmail.com>
2022-01-27 23:49:51 +0200

    init

---
+++ TestProject1/ApiRequest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using OpenQA.Selenium;
+using System.IO;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace TestProject1
+{
+    public class ApiRequest
+    {
+        public static IRestResponse SendAPIRequest(object body, Dictionary<string, string> headers, string url, Method type)
+        {
+            RestClient client = new RestClient(url)
+            {
+                Timeout = 300000
+            };
+
+            RestRequest request = new RestRequest(type);
+
+            foreach (var header in headers)
+            {
+                request.AddHeader(header.Key, header.Value);
+            }
+
+            if (!headers.Any(h => h.Value.Contains("appllication/json")))
+            {
+                foreach(var data in (Dictionary<string, string>)body){
+                    request.AddParameter(data.Key,data.Value);
+                }
+            }
+            else
+            {
+                request.AddJsonBody(body);
+                request.RequestFormat = DataFormat.Json;
+            }
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public static Cookie ExtractCookie(IRestResponse response, string cookieName)
+        {
+            Cookie res = null;
+            foreach (var cookie in response.Cookies)
+            {
+                if (cookie.Name.Equals(cookieName))
+                {
+                    res = new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null);
+                }
+            }
+            return res;
+        }
+
+        public static List<Cookie> ExtractAllCookies(IRestResponse response)
+        {
+            List<Cookie> res = new List<Cookie>();
+            foreach(var cookie in response.Cookies)
+            {
+                res.Add(new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null));
+            }
+            return res;
+        }
+
+        public static void DownloadPicture(string url)
+        {
+            RestClient client = new RestClient(url);
+            var pictureResponse = new RestRequest(Method.GET);
+            byte[] result = client.DownloadData(pictureResponse);
+            File.WriteAllBytes(Path.Combine("/testPIC", "test1.jpg"), result); //downloads to D drive
+        }
+
+       
+
+        
+    }
+}
---
+++ TestProject1/ResponseHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace TestProject1
+{
+    public class ResponseHandler
+    {
+        public string Handler(string codeStatus)
+        {
+            //RestClient client = new RestClient(url);
+            //var request = new RestRequest(Method.GET);
+            //IRestResponse response = client.Execute(request);
+            var handlr = new Dictionary<string, string>
+            {
+                //{"",""}
+                {"Continue","101"},
+                {"Switching protocols","102"},
+                {"Early hints","103"},
+                {"OK","200"},
+                {"Created","201"},
+                {"Accepted","202"},
+                {"Non-Authoritative Information","203"},
+                {"No Content","204"},
+                {"Reset Content","205"},
+                {"Partial Content","206"},
+                {"Multiple Choices","300"},
+                {"Moved Permanntly","301"},
+                {"Moved Temporarily","302"},
+                {"See Other", "303"},
+                {"Not Modified","304"},
+                {"Temporary Redirected","307" },
+                {"Permanent Redirect","308"},
+                {"Bad Request","400"},
+                {"Unauthorized","401"},
+                {"Forbidden","403"},
+                {"Not found","404"},
+                {"Method not allowed","405"},
+                {"Not Acceptable","406"},
+                {"Internal Server Error","500"},
+                {"Not Implemented","501"},
+                {"Bad Gateway","502"},
+                {"Service Unavailable","503"},
+                {"Gateway Timeout","504"},
+                {"HTTP Version Not Supported","505" },
+                { "Network Authentication Required","511"}         
+
+            };
+           
+            foreach (var status in handlr)
+            {
+                if(codeStatus == status.Key)
+                {
+                    return status.Value;
+                }
+            }
+            return "";
+        }
+    }
+}
